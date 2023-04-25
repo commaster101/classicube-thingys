62fd3a277e26eb281648505cc0d6024d8a0316ba
LJplays <89606052+LJplayswasused@users.noreply.github.com>
2023-04-25 02:32:39 -0400

    Update illLogic.cs
    added flipper block a directions for the placer block

--- illLogic.cs
+++ illLogic.cs
@@ -7,22 +7,28 @@ using BlockID = System.UInt16;
 
 namespace MCGalaxy {
 	
-	public sealed class wires : Plugin {
+	public sealed class illLogic : Plugin {
 		public override string name { get { return "illLogic"; } }
 		public override string MCGalaxy_Version { get { return "1.9.4.1"; } }
         //This name is used to determine who to send debug text to
-        static string author = "LJplays";//used goodlyays physics exsample
+        static string author = "LJplays+";
 		public override string creator { get { return author; } }
         
         //The level we want to add a custom physics block to.
         static string physicsLevelName = "ljplays";
         
-        //This is a server-side block ID. Client-side this is 103.
+        //This is a server-side block IDs for all the blocks added
         static BlockID wireBlock = 359;
-        
+
+        static BlockID flipperBlock = 362;
+
         static BlockID placerDBlock = 361;
         static BlockID placerUBlock = 365;
-        
+        static BlockID placerNBlock = 373;
+        static BlockID placerSBlock = 374;
+        static BlockID placerEBlock = 375;
+        static BlockID placerWBlock = 376;
+
         static BlockID lightOnBlock = 363;
         static BlockID lightOffBlock = 364;
 
@@ -43,10 +49,16 @@ namespace MCGalaxy {
 			foreach (Level lvl in levels) {
                 if (lvl.name == physicsLevelName) {
                     lvl.PhysicsHandlers[wireBlock] = DoWire;
-                    
-                    lvl.PhysicsHandlers[placerDBlock] = DoPlacerD;
-                    lvl.PhysicsHandlers[placerUBlock] = DoPlacerU;
-                    
+
+                    lvl.PhysicsHandlers[flipperBlock] = DoFlipper;
+
+                    lvl.PhysicsHandlers[placerDBlock] = DoPlacer;
+                    lvl.PhysicsHandlers[placerUBlock] = DoPlacer;
+                    lvl.PhysicsHandlers[placerNBlock] = DoPlacer;
+                    lvl.PhysicsHandlers[placerSBlock] = DoPlacer;
+                    lvl.PhysicsHandlers[placerEBlock] = DoPlacer;
+                    lvl.PhysicsHandlers[placerWBlock] = DoPlacer;
+
                     lvl.PhysicsHandlers[lightOnBlock] = Dolight;
                     lvl.PhysicsHandlers[lightOffBlock] = Dolight;
 
@@ -56,6 +68,7 @@ namespace MCGalaxy {
                     lvl.PhysicsHandlers[weyWBlock] = Dowey;
                     lvl.PhysicsHandlers[weyUBlock] = Dowey;
                     lvl.PhysicsHandlers[weyDBlock] = Dowey;
+
                 }
 			}
             
@@ -71,10 +84,16 @@ namespace MCGalaxy {
 		static void OnBlockHandlersUpdated(Level lvl, BlockID block) {
             if (lvl.name != physicsLevelName) { return; }
             if (block == wireBlock) lvl.PhysicsHandlers[wireBlock] = DoWire;
-           
-            if (block == placerDBlock) lvl.PhysicsHandlers[placerDBlock] = DoPlacerD;
-            if (block == placerUBlock) lvl.PhysicsHandlers[placerUBlock] = DoPlacerU;
-            
+
+            if (block == flipperBlock) lvl.PhysicsHandlers[flipperBlock] = DoFlipper;
+
+            if (block == placerDBlock) lvl.PhysicsHandlers[placerDBlock] = DoPlacer;
+            if (block == placerUBlock) lvl.PhysicsHandlers[placerUBlock] = DoPlacer;
+            if (block == placerNBlock) lvl.PhysicsHandlers[placerNBlock] = DoPlacer;
+            if (block == placerSBlock) lvl.PhysicsHandlers[placerSBlock] = DoPlacer;
+            if (block == placerEBlock) lvl.PhysicsHandlers[placerEBlock] = DoPlacer;
+            if (block == placerWBlock) lvl.PhysicsHandlers[placerWBlock] = DoPlacer;
+
             if (block == lightOnBlock) lvl.PhysicsHandlers[lightOnBlock] = Dolight;
             if (block == lightOffBlock) lvl.PhysicsHandlers[lightOffBlock] = Dolight;
 
@@ -82,6 +101,8 @@ namespace MCGalaxy {
             if (block == weySBlock) lvl.PhysicsHandlers[weySBlock] = Dowey;
             if (block == weyEBlock) lvl.PhysicsHandlers[weyEBlock] = Dowey;
             if (block == weyWBlock) lvl.PhysicsHandlers[weyWBlock] = Dowey;
+            if (block == weyUBlock) lvl.PhysicsHandlers[weyUBlock] = Dowey;
+            if (block == weyDBlock) lvl.PhysicsHandlers[weyDBlock] = Dowey;
         }
 
         void toggles(Player p, MouseButton button, MouseAction action, ushort yaw, ushort pitch, byte entity, ushort x, ushort y, ushort z, TargetBlockFace face)
@@ -94,6 +115,50 @@ namespace MCGalaxy {
             Dotoggle(p, x, y, z);
         }
 
+        static void DoFlipper(Level lvl, ref PhysInfo C)
+        {
+            ushort x = C.X, y = C.Y, z = C.Z;
+            int index = C.Index;
+
+            int f = 0;
+            if (lvl.GetBlock(x, (ushort)(y + 1), z) != 0) f++;
+            if (lvl.GetBlock(x, (ushort)(y - 1), z) != 0) f--;
+
+            if (f == 0){
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+                return;
+            }
+
+            if(lvl.GetBlock(x, (ushort)(y + f + f), z) != 0) return;
+            if (lvl.GetBlock(x, (ushort)(y + f), z) == flipperBlock) return;
+            
+            lvl.AddUpdate(C.Index, Block.Air, default(PhysicsArgs)); 
+            lvl.AddUpdate(lvl.IntOffset(index, 0, +f, 0), C.Block);
+
+            BlockID push = lvl.GetBlock(x, (ushort)(y + f), z);
+            lvl.AddUpdate(lvl.IntOffset(index, 0, +f +f, 0), push);
+
+            //push players
+            if (f == 1)
+            {
+                Player[] players = PlayerInfo.Online.Items;
+                foreach (Player p in players)
+                {
+                    if (p.Pos.BlockY == (C.Y + 2) || p.Pos.BlockY == (C.Y + 3))
+                    {
+                        if((p.Pos.BlockZ == C.Z) && p.Pos.BlockX == C.X)
+                        {
+                            //this looks kind wierd but we are tping a the player up
+                            Position pos = Position.FromFeet(p.Pos.X, p.Pos.Y, p.Pos.Z);
+                            p.SendPosition(pos, p.Rot);
+                        }
+                    }
+                }
+            }
+            C.Data.Data = PhysicsArgs.RemoveFromChecks;
+        }
+    
+
         static void Dowey(Level lvl, ref PhysInfo C)
         {
             ushort x = C.X, y = C.Y, z = C.Z;
@@ -103,32 +168,32 @@ namespace MCGalaxy {
             int index;
 
             BlockID block = lvl.GetBlock(x, y, z, out index);
-            if (block == 369)//+x
+            if (block == weyEBlock)//+x
             {
                 lvl.GetBlock((ushort)(x + 1), y, z, out index);
                 lvl.AddCheck(index);
             }
-            else if (block == 370)//-x
+            else if (block == weyWBlock)//-x
             {
                 lvl.GetBlock((ushort)(x - 1), y, z, out index);
                 lvl.AddCheck(index);
             }
-            else if (block == 368)//+z
+            else if (block == weySBlock)//+z
             {
                 lvl.GetBlock(x, y, (ushort)(z + 1), out index);
                 lvl.AddCheck(index);
             }
-            else if (block == 367)//-z
+            else if (block == weyNBlock)//-z
             { 
                 lvl.GetBlock(x, y, (ushort)(z - 1), out index);
                 lvl.AddCheck(index);
             }
-            else if (block == 371)//+y
+            else if (block == weyUBlock)//+y
             {
                 lvl.GetBlock(x, (ushort)(y + 1), z, out index);
                 lvl.AddCheck(index);
             }
-            else if (block == 372)//-y
+            else if (block == weyDBlock)//-y
             {
                 lvl.GetBlock(x, (ushort)(y - 1), z, out index);
                 lvl.AddCheck(index);
@@ -145,14 +210,14 @@ namespace MCGalaxy {
             int index;
 
             BlockID block = lvl.GetBlock(x, y, z, out index);
-            if (block == 363)
+            if (block == lightOnBlock)
             {
-                block = 364;
+                block = lightOffBlock;
                 lvl.AddUpdate(index, block, true);
             }
-            else if(block == 364) 
+            else if(block == lightOffBlock) 
             {
-                block = 363;
+                block = lightOnBlock;
                 lvl.AddUpdate(index, block, true);
 
                 lvl.GetBlock((ushort)(x + 1), y, z, out index);
@@ -167,12 +232,12 @@ namespace MCGalaxy {
                 lvl.GetBlock(x, y, (ushort)(z - 1), out index);
                 lvl.AddCheck(index);
 
-                if (lvl.GetBlock(x, (ushort)(y + 1), z) != 361 && (lvl.GetBlock(x, (ushort)(y + 1), z) != 365))
+                if (lvl.GetBlock(x, (ushort)(y + 1), z) != placerDBlock && (lvl.GetBlock(x, (ushort)(y + 1), z) != placerUBlock))
                 {
                     lvl.GetBlock(x, (ushort)(y + 1), z, out index);
                     lvl.AddCheck(index);
                 }
-                if (lvl.GetBlock(x, (ushort)(y - 1), z) != 361 && (lvl.GetBlock(x, (ushort)(y - 1), z) != 365))
+                if (lvl.GetBlock(x, (ushort)(y - 1), z) != placerDBlock && (lvl.GetBlock(x, (ushort)(y - 1), z) != placerUBlock))
                 {
                     lvl.GetBlock(x, (ushort)(y - 1), z, out index);
                     lvl.AddCheck(index);
@@ -181,28 +246,63 @@ namespace MCGalaxy {
             C.Data.Data = PhysicsArgs.RemoveFromChecks;
         }
 
-        static void DoPlacerU(Level lvl, ref PhysInfo C)
+        static void DoPlacer(Level lvl, ref PhysInfo C)
         {
             ushort x = C.X, y = C.Y, z = C.Z;
-            //int index = C.Index;
-            ushort yCur = y;
-            BlockID below = lvl.GetBlock(x, (ushort)(y - 1), z);
-            int index;
-            lvl.GetBlock(x, (ushort)(y + 1), z, out index);
-            lvl.AddUpdate(index, below, true);
-            C.Data.Data = PhysicsArgs.RemoveFromChecks;
-        }
-
-        static void DoPlacerD(Level lvl, ref PhysInfo C)
-        {
-            ushort x = C.X, y = C.Y, z = C.Z;
-            //int index = C.Index;
-            ushort yCur = y;
-            BlockID above = lvl.GetBlock(x, (ushort)(y + 1), z);
-            int index;
-            lvl.GetBlock(x, (ushort)(y - 1), z, out index);
-            lvl.AddUpdate(index, above, true);
-            C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            BlockID block = lvl.GetBlock(x, y, z);
+            if (block == placerUBlock) {
+                BlockID below = lvl.GetBlock(x, (ushort)(y - 1), z);
+                if (below == placerUBlock) return;
+                int index;
+                lvl.GetBlock(x, (ushort)(y + 1), z, out index);
+                lvl.AddUpdate(index, below, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
+            else if(block == placerDBlock)
+            {
+                BlockID above = lvl.GetBlock(x, (ushort)(y + 1), z);
+                if (above == placerDBlock) return;
+                int index;
+                lvl.GetBlock(x, (ushort)(y - 1), z, out index);
+                lvl.AddUpdate(index, above, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
+            else if(block == placerNBlock)
+            {
+                BlockID behind = lvl.GetBlock(x, y, (ushort)(z + 1));
+                if (behind == placerNBlock) return;
+                int index;
+                lvl.GetBlock(x, y, (ushort)(z - 1), out index);
+                lvl.AddUpdate(index, behind, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
+            else if(block == placerSBlock)
+            {
+                BlockID behind = lvl.GetBlock(x, y, (ushort)(z - 1));
+                if (behind == placerSBlock) return;
+                int index;
+                lvl.GetBlock(x, y, (ushort)(z + 1), out index);
+                lvl.AddUpdate(index, behind, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
+            else if(block == placerEBlock)
+            {
+                BlockID behind = lvl.GetBlock((ushort)(x - 1), y, z);
+                if (behind == placerEBlock) return;
+                int index;
+                lvl.GetBlock((ushort)(x + 1), y, z, out index);
+                lvl.AddUpdate(index, behind, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
+            else if(block == placerWBlock)
+            {
+                BlockID behind = lvl.GetBlock((ushort)(x + 1), y, z);
+                if (behind == placerWBlock) return;
+                int index;
+                lvl.GetBlock((ushort)(x - 1), y, z, out index);
+                lvl.AddUpdate(index, behind, true);
+                C.Data.Data = PhysicsArgs.RemoveFromChecks;
+            }
         }
 
         //basic wire can activate doors,leaves and sand 
@@ -232,12 +332,12 @@ namespace MCGalaxy {
             lvl.GetBlock(x, y, (ushort)(z - 1), out index);
             lvl.AddCheck(index);
 
-            if (lvl.GetBlock(x, (ushort)(y + 1), z) != 361 && (lvl.GetBlock(x, (ushort)(y + 1), z) != 365))
+            if (lvl.GetBlock(x, (ushort)(y + 1), z) != placerDBlock && (lvl.GetBlock(x, (ushort)(y + 1), z) != placerUBlock))
             {
                 lvl.GetBlock(x, (ushort)(y + 1), z, out index);
                 lvl.AddCheck(index);
             }
-            if (lvl.GetBlock(x, (ushort)(y - 1), z) != 361 && (lvl.GetBlock(x, (ushort)(y - 1), z) != 365)) 
+            if (lvl.GetBlock(x, (ushort)(y - 1), z) != placerDBlock && (lvl.GetBlock(x, (ushort)(y - 1), z) != placerUBlock)) 
             {
                 lvl.GetBlock(x, (ushort)(y - 1), z, out index);
                 lvl.AddCheck(index);
